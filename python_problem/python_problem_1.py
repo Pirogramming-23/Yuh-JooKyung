df49d53de9d5fbc6b5f654e1eb9aa8b6f768dd82
JooKyung-Yuh <johnlu0410@gmail.com>
2025-07-04 14:42:08 +0900

    python_problem_1 / 3단계: 정수 입력 및 1, 2, 3 입력 처리

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,11 @@
 num = 0
 
-num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+while(True):   
+    try:
+        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+        if num == 1 or num == 2 or num == 3:
+            break
+        else:
+            print("1, 2, 3 중 하나를 입력하세요")
+    except ValueError:
+        print("정수를 입력하세요")  # ValueError가 발생하면 수행
