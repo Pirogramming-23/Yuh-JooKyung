34d1c2c18e56054ca79558aa9c6975a36e4d4a64
JooKyung-Yuh <johnlu0410@gmail.com>
2025-07-04 14:58:12 +0900

    python_problem_1 / 5단계: 변수 num을 이용하여 입력한 수만큼 숫자를 출력하는 코드를 작성하여라.

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,22 +1,35 @@
 num = 0
 
-while(True):   
-    try:
-        N = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-        if N == 1 or N == 2 or N == 3:
-            break
-        else:
-            print("1, 2, 3 중 하나를 입력하세요")
-    except ValueError:
-        print("정수를 입력하세요")  # ValueError가 발생하면 수행
+def input_num():
+    while(True):   
+        try:
+            N = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+            if N == 1 or N == 2 or N == 3:
+                break
+            else:
+                print("1, 2, 3 중 하나를 입력하세요")
+        except ValueError:
+            print("정수를 입력하세요")  # ValueError가 발생하면 수행
+    return N
 
-for i in range(N):
-    num += 1
-    print("PlayerA : ", num)
+def player_A(N, num):
+    for i in range(N):
+        num += 1
+        print("PlayerA : ", num)
+
+def player_B(N, num):
+    for i in range(N):
+        num += 1
+        print("PlayerB : ", num)
+
+def main():
+    while(True):
+        N = input_num()
+        player_A(N, num)
+        M = input_num()
+        player_B(M, num) 
 
 
-
-
-
+main()
 
 
