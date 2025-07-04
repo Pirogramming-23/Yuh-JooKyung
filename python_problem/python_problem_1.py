8c27dd8c2564d858ee14e63714fa9694e5733621
JooKyung-Yuh <johnlu0410@gmail.com>
2025-07-04 15:39:46 +0900

    python_problem_1 / 8단계: 6단계까지 중복되는 코드를 찾아 함수로 만들어라. 이때, 함수 이름은 brGame으로 한다.

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,6 +1,7 @@
 num = 0
 
-def input_num():
+def brGame():
+    global num
     while(True):   
         try:
             N = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
@@ -10,35 +11,44 @@ def input_num():
                 print("1, 2, 3 중 하나를 입력하세요")
         except ValueError:
             print("정수를 입력하세요")  # ValueError가 발생하면 수행
-    return N
-
-def player_A(N):
-    global num
+    
+    # 입력받은 수만큼 숫자 출력
     for i in range(N):
         num += 1
-        print("PlayerA : ", num)
-
-def player_B(N):
-    global num
-    for i in range(N):
+        print("playerA : ", num)
+        if num == 31:
+            return "A"
+    
+    # playerB 차례
+    while(True):   
+        try:
+            M = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+            if M == 1 or M == 2 or M == 3:
+                break
+            else:
+                print("1, 2, 3 중 하나를 입력하세요")
+        except ValueError:
+            print("정수를 입력하세요")
+    
+    # 입력받은 수만큼 숫자 출력
+    for i in range(M):
         num += 1
-        print("PlayerB : ", num)
+        print("playerB : ", num)
+        if num == 31:
+            return "B"
+    
+    return None
 
 def main():
     global num
     while(True):
-        N = input_num()
-        player_A(N)
-        if num == 31:
-            print("playerB win!")
-            break
-
-        M = input_num()
-        player_B(M) 
-        if num == 31:
+        winner = brGame()
+        if winner == "A":
             print("playerA win!")
             break
-
+        elif winner == "B":
+            print("playerB win!")
+            break
 
 main()
 
