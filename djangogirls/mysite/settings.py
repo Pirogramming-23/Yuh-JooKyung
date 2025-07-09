d5c806f970fa838dfef2ebba629cf6229e58b7ee
JooKyung-Yuh <johnlu0410@gmail.com>
2025-07-10 04:22:21 +0900

    Django 모델

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
